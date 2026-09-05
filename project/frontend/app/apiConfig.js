5e232b0ec0524d2b88ded41006ff9fba69e15aa5
Kwame Ofosu Grant <grantjnr@users.noreply.github.com>
2026-09-05 09:51:07 +0000

    added project documentation

--- project/frontend/app/apiConfig.js
+++
@@ -1,8 +0,0 @@
-export const API_BASE_URL = 'http://192.168.1.77:8000';
-
-export const buildApiUrl = (path = '') => {
-  if (!path) return API_BASE_URL;
-  return `${API_BASE_URL}${path.startsWith('/') ? path : `/${path}`}`;
-};
-
-export const API_PREDICT_URL = buildApiUrl('/api/predict/');
